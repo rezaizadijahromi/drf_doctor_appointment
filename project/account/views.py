c86417773dc4813210292095cf82d1fd30eae392
rezaizadijahromi <rezaizadij2000@gmail.com>
2021-12-22 00:05:39 +0330

    Fix issue with drf

--- project/account/views.py
+++ project/account/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+## Python modules
+import datetime
+import uuid
+import random
+import os.path
 
-# Create your views here.
+## email validator packege
+from email_validator import validate_email, EmailNotValidError
+
+## django imports
+from django.shortcuts import render
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+
+## django restframework
+from rest_framework import permissions, views
+
+def email_validator(email):
+    try:
+        validate_email_data = validate_email(email)
+        email_add = validate_email_data['email']
+        return email_add
+    except EmailNotValidError as e:
+        return str(e)
+
+class RegisterView(views.APIView):
+    pass
--- project/project/settings.py
+++ project/project/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    "account",
     "rest_framework",
-    "account"
 ]
 
 MIDDLEWARE = [
