b5f6ee6a0c48cdc33427a059d966c17c5d65e6e0
rezaizadijahromi <rezaizadij2000@gmail.com>
2021-12-22 10:14:10 +0330

    add users, register, login

--- project/account/urls.py
+++ project/account/urls.py
@@ -1,11 +1,20 @@
 from django import urls
-from django.contrib import admin
 from django.urls import path
+
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+
 
 from . import views
 
 app_name = "account"
 
 urlpatterns = [
+    path('', views.users, name='users'),
+    path('profile/', views.Profile.as_view(), name='profile'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('login/', views.MyTokenObtainPairView.as_view(), name='login'),
+    path('refresh_token/', TokenRefreshView.as_view(), name='refresh_token'),
 ]
 
--- project/account/views.py
+++ project/account/views.py
@@ -11,10 +11,23 @@ from email_validator import validate_email, EmailNotValidError
 from django.shortcuts import render
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
+from django.db.models import Q , Count
 
 ## django restframework
-from rest_framework import permissions, views, status
+from rest_framework import permissions, serializers, views, status
 from rest_framework.views import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.pagination import PageNumberPagination
+
+## drf jwt
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+## app imports
+from .models import SkillTag, TopicTag, UserProfile
+from .serializers import (UserProfileSerializer, UserSerializer,
+                          UserSerializerWithToken, CurrentUserSerializer)
+
 
 def email_validator(email):
     try:
@@ -46,7 +59,7 @@ class RegisterView(views.APIView):
             messages['errors'].append("password can't be empty")
         if User.objects.filter(email=email).exists():
             messages['errors'].append("account already exists with this email id")
-        if User.objects.filter(username_iexact=username).exists():
+        if User.objects.filter(username__iexact=username).exists():
             messages['errors'].append(("Acount already exists with this username"))
         if len(messages['errors']) > 0:
             return Response({"detail":messages['errors']},status=status.HTTP_400_BAD_REQUEST)
@@ -61,4 +74,53 @@ class RegisterView(views.APIView):
         except Exception as e:
             return Response({'detail':f'{e}'},status=status.HTTP_400_BAD_REQUEST)
 
+        return Response(serializer.data)
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['username'] = user.username
+        token['name'] = user.userprofile.name
+        token['profile_pic'] = 'static' + user.userprofile.profile_pic.url
+        token['is_staff'] = user.is_staff
+        token['id'] = user.id
+
+        return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        serializer = UserSerializerWithToken(self.user).data
+        for k, v in serializer.items():
+            data[k] = v
+
+        return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+@api_view(['GET'])
+def users(request):
+    query = request.query_params.get('q') or ''
+    users = User.objects.filter(
+        Q(userprofile__name__icontains=query) | 
+        Q(userprofile__username__icontains=query)
+    )
+    paginator = PageNumberPagination()
+    paginator.page_size = 10
+    result_page = paginator.paginate_queryset(users,request)
+    serializer = UserSerializer(result_page, many=True)
+    return paginator.get_paginated_response(serializer.data)
+
+
+class Profile(views.APIView):
+    
+    peromission_classes = [permissions.IsAuthenticated]
+    
+    def get(self, request):
+        serializer = UserSerializer(request.user, many=False)
         return Response(serializer.data)
