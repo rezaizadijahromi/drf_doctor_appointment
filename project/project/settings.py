29a65ca801c3f82623551976f1c8872419c2b82f
rezaizadijahromi <rezaizadij2000@gmail.com>
2021-12-25 12:24:32 +0330

    Add email config

--- project/project/settings.py
+++ project/project/settings.py
@@ -161,6 +161,14 @@ USE_I18N = True
 
 USE_TZ = True
 
+# SMPT Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'punisherrij@gmail.com'
+EMAIL_HOST_PASSWORD = 'punisher1234'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
