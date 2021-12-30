c7abc68c7693bf1ffd7603e3a70f66f5d532331b
rezaizadijahromi <rezaizadij2000@gmail.com>
2021-12-30 19:19:32 +0330

    add filter and better status display for admin panel

--- project/booking/admin.py
+++ project/booking/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Booking, Room
 
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = [
+        'booking_date', 'start_timing', 'end_timing', 'admin_did_accept', 'is_pending',
+    ]
+
+    list_filter = [
+        'booking_date', 'start_timing', 'end_timing', 'admin_did_accept', 'is_pending',
+    ] 
+
+
 admin.site.register(Room)
-admin.site.register(Booking)
+admin.site.register(Booking, BookingAdmin)
