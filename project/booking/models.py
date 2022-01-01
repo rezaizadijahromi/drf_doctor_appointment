5a6e3849187f49f8fa74215921f9bd000480a3d0
rezaizadijahromi <rezaizadij2000@gmail.com>
2022-01-01 23:09:05 +0330

    change the booking model

--- project/booking/models.py
+++ project/booking/models.py
@@ -11,19 +11,19 @@ class Room(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     room_name = models.CharField(max_length=255, blank=False, null=False)
     description = models.TextField(null=True, blank=True)
+    doctor = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, blank=True, related_name="doctor")
 
     def __str__(self):
         return f'{self.room_name} with id {self.id} is full'
 
 class Booking(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-    doctor = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, blank=True, related_name="doctor")
     patient = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, blank=True, related_name="patient")
     room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name="booking")
     booking_date = models.DateField(default=datetime.date.today)
     start_timing = models.TimeField()
     end_timing = models.TimeField()
-    purpose_of_booking = models.TextField()
+    purpose_of_booking = models.TextField(blank=True, null=True, default=None)
     is_pending = models.BooleanField(default=False)
     admin_did_accept = models.BooleanField(default=False)
     admin_feedback = models.TextField(blank=True, null=True, default=None)
