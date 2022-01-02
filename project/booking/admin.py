138def9796fa9deaba8768496e2cee3a13304221
rezaizadijahromi <rezaizadij2000@gmail.com>
2022-01-02 12:13:30 +0330

    update damin page

--- project/booking/admin.py
+++ project/booking/admin.py
@@ -4,11 +4,11 @@ from .models import Booking, Room
 
 class BookingAdmin(admin.ModelAdmin):
     list_display = [
-        'booking_date', 'start_timing', 'end_timing', 'admin_did_accept', 'is_pending',
+        'booking_date', 'start_timing', 'end_timing', 'admin_did_accept', 'is_pending', "room",
     ]
 
     list_filter = [
-        'booking_date', 'start_timing', 'end_timing', 'admin_did_accept', 'is_pending',
+        'booking_date', 'start_timing', 'end_timing', 'admin_did_accept', 'is_pending', "room",
     ] 
 
 
