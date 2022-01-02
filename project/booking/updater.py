1b98d6532dfa954cd895452e3dd5599411d1adff
rezaizadijahromi <rezaizadij2000@gmail.com>
2022-01-02 11:35:21 +0330

    add automation for accept requests

--- project/booking/updater.py
+++ project/booking/updater.py
@@ -16,7 +16,7 @@ from users.models import UserProfile
 scheduler = BackgroundScheduler()
 
 
-def update_req():
+def send_reminder():
     try:
         c_date = "2021-12-26"
 
@@ -31,11 +31,6 @@ def update_req():
             # print(now_5_10.time())
 
             for b in booking:
-
-                print("here we are1")
-
-
-
                 user = UserProfile.objects.get(
                     username=b.patient
                 )
@@ -60,36 +55,46 @@ def update_req():
 
                     email.send()
 
-
-
     except Exception as e:
         print("Some error occured: "+str(e))
 
+def accpet_request():
+    try:
+        now = datetime.datetime.now()
+        rounded = (now - (now - datetime.min) % datetime.timedelta(minutes=30)).strftime("%H:%M")
+
+        print(rounded)
+
+        for room in Room.objects.all():
+            booking = Booking.objects.filter(
+                is_pending=True, booking_date=datetime.date.today(),room=room
+            )
+
+            if not booking.exists():
+                print("No data to take action upon")
+            else:
+                slots = booking.filter(start_timing=rounded)
+                if not slots.exists():
+                    print("This slot doesn't have any pending request") 
+                else:
+                    y = min(slots.values_list('created_at', flat=True))
+                    accept = slots.get(created_at=y)
+                    accept.admin_did_accept = True
+                    accept.is_pending = False
+                    accept.admin_feedback = "Accepted by automation"
+                    accept.save()
+                    reject = slots.exclude(id=accept.id)
+                    feedback = "Declined on first come first serve basis"
+                    reject.update(
+                        admin_did_accept=False,
+                        is_pending=False, admin_feedback=feedback
+                    )
+
+    except Exception as e:
+        print(f"Error Accept: {e}")
+
 
 def startfunc():
-    scheduler.add_job(update_req, "interval", start_date="2022-01-01 09:30:00", end_date="2022-01-01 12:30:00",minutes=10)
+    scheduler.add_job(send_reminder, "interval",minutes=2)
+    scheduler.add_job(accpet_request, "interval",minutes=2)
     scheduler.start()
-
-
-# x = datetime.now()
-# rounded = (x - (x - datetime.min) % timedelta(minutes=30)).strftime("%H:%M")
-# for room in Room.objects.all():
-#     booking = Booking.objects.filter(is_pending=True, booking_date=date.today(), Room=room)
-#     # Handle empty slots
-#     if not booking.exists():
-#         print("no data to take action upon")
-#     else:
-#         slots = booking.filter(start_timing=rounded)
-#         if not slots.exists():
-#             print("This slot doesn't have any pending requests")
-#         else:
-#             y = min(slots.values_list('created_at', flat=True))
-#             accept = slots.get(created_at=y)
-#             accept.admin_did_accept = True
-#             accept.is_pending = False
-#             accept.admin_feedback = "Accepted on first come first server basis"
-#             accept.save()
-#             reject = slots.exclude(id=accept.id)
-#             feedback = "Declined on first come first serve basis"
-#             reject.update(admin_did_accept=False,
-#                           is_pending=False, admin_feedback=feedback)
