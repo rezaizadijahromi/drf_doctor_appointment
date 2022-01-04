398d8e9355f6be68831611173792a3ef8c6a8434
rezaizadijahromi <rezaizadij2000@gmail.com>
2022-01-04 16:20:01 +0330

    room detail(slotList) initialized

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignIn from "./AuthComponents/SignIn";
 import SignUp from "./AuthComponents/SignUp";
 import Home from "./CoreComponent/home";
+import SlotList from "./CoreComponent/slotsList";
 import Navbar from "./StyleComponent/Navbar";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="/:id" element={<SlotList />}></Route>
           <Route path="/signin" element={<SignIn />}></Route>
           <Route path="/signup" element={<SignUp />}></Route>
         </Routes>
