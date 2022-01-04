89e329db3f50a4926aa3fd84e9b03a7f5cff7f79
rezaizadijahromi <rezaizadij2000@gmail.com>
2022-01-04 16:19:31 +0330

    Fix home component

--- frontend/src/CoreComponent/home.js
+++ frontend/src/CoreComponent/home.js
@@ -5,16 +5,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
 import { apiConfig } from "../config";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import Icon from "@material-ui/core/Icon";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogTitle from "@material-ui/core/DialogTitle";
 import { CardMedia, Container, Grid } from "@material-ui/core";
 import { NavLink } from "react-router-dom";
 
