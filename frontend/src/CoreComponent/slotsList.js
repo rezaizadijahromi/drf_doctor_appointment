668641a0b9d82ac04957fa2f762b5f33ad43598b
rezaizadijahromi <rezaizadij2000@gmail.com>
2022-01-05 20:44:54 +0330

    working on slot list doctor info

--- frontend/src/CoreComponent/slotsList.js
+++ frontend/src/CoreComponent/slotsList.js
@@ -1,31 +1,24 @@
 import axios from "axios";
-import { Link } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 // import { makeStyles } from "@material-ui/core/styles";
-import { makeStyles, useTheme } from "@mui/styles";
+import { makeStyles } from "@mui/styles";
 import { apiConfig } from "../config";
-import { Box, ButtonGroup, Grid, Paper, Button } from "@mui/material";
+import {
+  Box,
+  ButtonGroup,
+  Grid,
+  Paper,
+  Button,
+  TextField,
+} from "@mui/material";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
 // import AdapterDateFns from "@mui/lab/AdapterDateFns";
-// import LocalizationProvider from "@mui/lab/LocalizationProvider";
-// import DateTimePicker from "@mui/lab/DateTimePicker";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import DateTimePicker from "@mui/lab/DateTimePicker";
 
 import { useParams } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
-  card: {
-    maxWidth: 600,
-    margin: "auto",
-    textAlign: "center",
-    marginTop: theme.spacing(5),
-    paddingBottom: theme.spacing(2),
-  },
-  error: {
-    verticalAlign: "middle",
-  },
-  title: {
-    marginTop: theme.spacing(2),
-    color: theme.palette.openTitle,
-  },
   textField: {
     marginLeft: theme.spacing(1),
     marginRight: theme.spacing(1),
@@ -35,9 +28,7 @@ const useStyles = makeStyles((theme) => ({
     margin: "auto",
     marginBottom: theme.spacing(2),
   },
-  appBar: {
-    borderBottom: `1px solid ${theme.palette.divider}`,
-  },
+
   link: {
     margin: theme.spacing(1, 1.5),
   },
@@ -49,39 +40,36 @@ const useStyles = makeStyles((theme) => ({
     width: theme.spacing(75),
     height: theme.spacing(55),
   },
+  paper1: {
+    margin: theme.spacing(2),
+    width: theme.spacing(75),
+    height: theme.spacing(55),
+  },
   outerbox: {
     height: "100%",
-    display: "flex",
+    display: "inline-block",
     flexDirection: "column",
-    alignItems: "flex-end",
-    justifyContent: "flex-end",
+    // alignItems: "flex-end",
+    // justifyContent: "flex-end",
   },
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120,
+  outerbox1: {
+    height: "100%",
+    display: "inline-block",
+    flexDirection: "column",
+    // alignItems: "flex-start",
+    // justifyContent: "flex-start",
   },
-  selectEmpty: {
-    marginTop: theme.spacing(2),
-  },
-  url_input: {
-    "& > *": {
-      margin: theme.spacing(1),
-      width: "25ch",
-    },
-  },
-  url_prefix: {
-    color: "#a8b1c7",
-  },
-  divider: {
-    height: 28,
-    margin: 4,
+
+  rightBox: {
+    display: "grid",
+    gridTemplateColumns: "repeat(2, 1fr)",
   },
 }));
 
 const SlotList = ({ match }) => {
   const classes = useStyles();
 
-  const [date, setDate] = useState("");
+  const [date, setDate] = useState(new Date(""));
   const [value, setValue] = useState([
     {
       id: "",
@@ -109,40 +97,81 @@ const SlotList = ({ match }) => {
         config,
       );
       setValue(data.data.data);
+      setDate(data.data.date);
+
+      console.log(data.data);
     }
   };
 
-  const handelDate = (e) => {
-    setDate({
-      date: e.target.value,
-    });
+  const handelDate = (newVal) => {
+    setDate(newVal);
   };
+
   useEffect(() => {
     slotListData();
   }, []);
 
+  console.log(date);
+
   return (
-    <Box className={classes.outerbox}>
-      <Paper className={classes.paper} elevation={3}>
-        <Box m={3}>
-          <Grid item xs={12}>
-            <Grid container justifyContent="center" spacing={2}>
-              {value.map((slots, index) => (
-                <Grid key={value} item>
-                  <ButtonGroup
-                    variant="contained"
-                    aria-label="outlined primary button group">
-                    <Button>
-                      {slots.start_timing} - {slots.end_timing}
-                    </Button>
-                  </ButtonGroup>
+    <>
+      <div className={classes.rightBox}>
+        <Box className={classes.outerbox1}>
+          <Paper className={classes.paper1} elevation={1}>
+            <Box m={3}>
+              <Grid item xs={12}>
+                <Grid container justifyContent="center" spacing={2}>
+                  <LocalizationProvider dateAdapter={AdapterDateFns}>
+                    <DateTimePicker
+                      label="Date&Time picker"
+                      value={value}
+                      onChange={handelDate}
+                      renderInput={(params) => <TextField {...params} />}
+                    />
+                  </LocalizationProvider>
                 </Grid>
-              ))}
-            </Grid>
-          </Grid>
+              </Grid>
+            </Box>
+          </Paper>
         </Box>
-      </Paper>
-    </Box>
+
+        {value.length > 0 ? (
+          <Box className={classes.outerbox}>
+            <Paper className={classes.paper} elevation={3}>
+              <Box m={3}>
+                <Grid item xs={12}>
+                  <Grid container justifyContent="center" spacing={2}>
+                    {value.map((slots, index) => (
+                      <Grid key={index} item>
+                        <ButtonGroup
+                          variant="contained"
+                          aria-label="outlined primary button group">
+                          <Button>
+                            {slots.start_timing} - {slots.end_timing}
+                          </Button>
+                        </ButtonGroup>
+                      </Grid>
+                    ))}
+                  </Grid>
+                </Grid>
+              </Box>
+            </Paper>
+          </Box>
+        ) : (
+          <Box className={classes.outerbox}>
+            <Paper className={classes.paper} elevation={3}>
+              <Box m={3}>
+                <Grid item xs={12}>
+                  <Grid container justifyContent="center" spacing={2}>
+                    No Appointment
+                  </Grid>
+                </Grid>
+              </Box>
+            </Paper>
+          </Box>
+        )}
+      </div>
+    </>
   );
 };
 
