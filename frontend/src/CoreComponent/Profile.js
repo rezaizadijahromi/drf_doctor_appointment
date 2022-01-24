9abb7bd7495e5ffeaa9d44c2d903fb4b618df791
rezaizadijahromi <rezaizadij2000@gmail.com>
2022-01-24 22:13:02 +0330

    add change avatar function

--- frontend/src/CoreComponent/Profile.js
+++ frontend/src/CoreComponent/Profile.js
@@ -2,18 +2,39 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { apiConfig } from "../config";
 import { Form } from "react-bootstrap";
+import { Button, Input } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 
 import Message from "../Component/Message";
 import Loader from "../Component/Loader";
 import "./profile.css";
 import { CardMedia } from "@mui/material";
+import { textAlign } from "@mui/system";
 
-{
-	/* {message && <Message variant="danger">{message}</Message>}
-					{success && <Message variant="success">Profile Updated</Message>} */
-}
+const useStyles = makeStyles((theme) => ({
+	submit: {
+		marginTop: 10,
+		right: "10%",
+		marginBottom: theme.spacing(2),
+		width: 200,
+	},
+	updateButton: {
+		width: 200,
+		marginRight: 30,
+		textAlign: "center",
+	},
+	buttons: {
+		width: 165,
+		height: 45,
+	},
+	alignment: {
+		textAlign: "center",
+	},
+}));
 
 const Profile = () => {
+	const classes = useStyles();
+
 	const [username, setUsername] = useState("");
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("********");
@@ -47,20 +68,55 @@ const Profile = () => {
 		}
 	};
 
-	const uploadFileHandler = async (e) => {
+	const changeEmail = async () => {
+		const userLocal = JSON.parse(localStorage.getItem("userInfo"));
+
+		if (userLocal) {
+			const config = {
+				headers: {
+					Authorization: `Bearer ${userLocal.data.access}`,
+				},
+			};
+
+			const response = await axios.patch(
+				`${apiConfig.baseUrl}/users/profile/`,
+				{ email: email },
+				config
+			);
+
+			if (response.data.success) {
+				console.log(response.data);
+				setEmail(response.data.updated_email);
+			} else {
+				console.log(response.data.message);
+			}
+		}
+	};
+
+	const profilePicChange = async (e) => {
+		const userLocal = JSON.parse(localStorage.getItem("userInfo"));
+
 		const file = e.target.files[0];
 		const formData = new FormData();
-		formData.append("image", file);
+		formData.append("profile_pic", file);
 		try {
 			const config = {
 				headers: {
+					Authorization: `Bearer ${userLocal.data.access}`,
 					"Content-Type": "multipart/form-data",
 				},
 			};
-			const { data } = await axios.post("/api/upload", formData, config);
-			setImage(data);
+
+			const response = await axios.patch(
+				`${apiConfig.baseUrl}/users/profile_update/photo/`,
+				formData,
+				config
+			);
+
+			// setImage(response.data);
+			console.log(response.data);
 		} catch (error) {
-			console.error(error);
+			console.log(error);
 		}
 	};
 
@@ -70,12 +126,12 @@ const Profile = () => {
 
 	return (
 		<>
-			<div class="userInformation">
-				<div class="top-text">User Information:</div>
+			<div className="userInformation">
+				<div className="top-text">User Information:</div>
 
-				<div class="half-div">
-					<div class="information">
-						<div class="info">Username</div>
+				<div className="half-div">
+					<div className="information">
+						<div className="info">Username</div>
 						<Form.Control
 							className="intext"
 							type="text"
@@ -83,14 +139,14 @@ const Profile = () => {
 							onChange={(e) => setUsername(e.target.value)}
 						></Form.Control>
 
-						<div class="info">Email</div>
+						<div className="info">Email</div>
 						<Form.Control
 							type="email"
 							value={email}
 							onChange={(e) => setEmail(e.target.value)}
 						></Form.Control>
 
-						<div class="info">Password</div>
+						<div className="info">Password</div>
 						<Form.Control
 							className="intext"
 							type="text"
@@ -98,7 +154,7 @@ const Profile = () => {
 							onChange={(e) => setPassword(e.target.value)}
 						></Form.Control>
 
-						<div class="info">Confirm your password</div>
+						<div className="info">Confirm your password</div>
 						<Form.Control
 							className="intext"
 							type="text"
@@ -106,43 +162,43 @@ const Profile = () => {
 							onChange={(e) => setPassword(e.target.value)}
 						></Form.Control>
 
-						<div>
-							<button
-								class="
-                      button
-                      btn btn-lg btn-primary btn-submit
-                      fw-bold
-                      text-uppercase
-                    "
-								type="submit"
+						<div className={classes.alignment}>
+							<Button
+								color="primary"
+								variant="contained"
+								onClick={changeEmail}
+								className={classes.updateButton}
 							>
 								Update
-							</button>
+							</Button>
 						</div>
 					</div>
 
-					<div class="forimg">
+					<div className="forimg">
 						<CardMedia component="img" src={image}></CardMedia>
-						{/*             
-						<Form.File
-							id="image-file"
-							label="Choose File"
-							custom
-							onChange={uploadFileHandler}
-						></Form.File> */}
-						{/* {uploading && <Loader />} */}
-						<button
-							class="
-                     button-avatar
-                      button-main-avatar
-                      btn btn-lg 
-                      fw-bold
-                      text-uppercase
-                    "
-							type="submit"
-						>
-							Change the profile picture
-						</button>
+
+						<label htmlFor="contained-button-file">
+							<Button
+								style={{
+									marginTop: 10,
+									right: "10%",
+									width: 200,
+								}}
+								component="span"
+								variant="contained"
+								onChange={profilePicChange}
+							>
+								<Input
+									accept="image/*"
+									id="contained-button-file"
+									multiple
+									type="file"
+									hidden
+									style={{ display: "none" }}
+								/>
+								upload
+							</Button>
+						</label>
 					</div>
 				</div>
 			</div>
