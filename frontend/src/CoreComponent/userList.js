711b34dee7208eb12f64c288c4b0db09d218f229
rezaizadijahromi <rezaizadij2000@gmail.com>
2022-01-25 23:18:52 +0330

    Fix fetching data to usersList

--- frontend/src/CoreComponent/userList.js
+++ frontend/src/CoreComponent/userList.js
@@ -10,19 +10,7 @@ import Loader from "../Component/Loader";
 import "./profile.css";
 
 const UserList = () => {
-	// const [users, setUsers] = useState([
-	// 	{
-	// 		results: {
-	// 			profile: {
-	// 				username: "",
-	// 				id: "",
-	// 			},
-	// 			email: "",
-	// 			isAdmin: false,
-	// 		},
-	// 	},
-	// ]);
-	var users = [
+	let [users, setUsers] = useState([
 		{
 			results: {
 				profile: {
@@ -33,7 +21,8 @@ const UserList = () => {
 				isAdmin: false,
 			},
 		},
-	];
+	]);
+
 	const userList = async () => {
 		const userLocal = JSON.parse(localStorage.getItem("userInfo"));
 
@@ -47,94 +36,22 @@ const UserList = () => {
 			const response = await axios.get(`${apiConfig.baseUrl}/users/`, config);
 
 			if (response.data) {
-				console.log();
-				for (let index = 0; index < response.data.results.length; index++) {
-					// console.log(response.data.results[index]);
-					users.push({
-						results: {
-							profile: {
-								id: response.data.results[index].profile.id,
-								username: response.data.results[index].profile.username,
-							},
-							email: response.data.results[index].email,
-							isAdmin: response.data.results[index].is_superuser,
-						},
-					});
-				}
-				return users;
+				setUsers(response.data.results);
 			}
 		}
 	};
 
-	console.log(userList());
 	useEffect(() => {
-		const usersData = userList().then((data) => {
-			console.log(data);
-		});
+		const usersData = userList();
 	}, []);
 
-	// for (let index = 0; index < users.length; index++) {
-	// 	console.log(users[index].is_superuser);
-	// }
-
+	console.log("users", users);
 	const deleteHandler = (id) => {
 		if (window.confirm("Are you sure?")) {
 		}
 	};
 
-	return (
-		<>
-			<h1> Users </h1>
-			<Table striped bordered hover responsive className="table-sm">
-				<thead>
-					<tr>
-						<th>ID</th>
-						<th>NAME</th>
-						<th>EMAIL</th>
-						<th>ADMIN</th>
-						<th></th>
-					</tr>
-				</thead>
-				<tbody>
-					{users.map((user) => (
-						<tr key={user.results.profile.id}>
-							<td>{user.results.profile.id}</td>
-							<td>{user.results.profile.username}</td>
-							<td>
-								<a href={`mailto:${user.results.email}`}>
-									{user.results.email}
-								</a>
-							</td>
-							<td>
-								{user.results.isAdmin ? (
-									<i
-										className="fas fa-check"
-										style={{
-											color: "green",
-										}}
-									></i>
-								) : (
-									<i className="fas fa-times" style={{ color: "red" }}></i>
-								)}
-							</td>
-							<td>
-								<Button variant="light" className="btn-sm">
-									<i className="fas fa-edit"></i>
-								</Button>
-								<Button
-									variant="danger"
-									className="btn-sm"
-									onClick={() => deleteHandler(user._id)}
-								>
-									<i className="fas fa-trash"></i>
-								</Button>
-							</td>
-						</tr>
-					))}
-				</tbody>
-			</Table>
-		</>
-	);
+	return <></>;
 };
 
 export default UserList;
